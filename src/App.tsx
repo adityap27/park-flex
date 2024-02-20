cc6e2ce678b7f3493750cf08fcca673ec0bc59df
Mann <mn906219@dal.ca>
2024-02-20 00:54:36 -0400

    Added Wallet Module

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./home-page/Home";
+import Home from "./home-page/Home"; 
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { SpotDetails } from "./spot-details/SpotDetails";
 import "react-calendar/dist/Calendar.css";
@@ -10,18 +10,20 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import CreateListing from "./create-listing/CreateListing";
 import { SuccessMessage } from "./components/SuccessMessage";
+import { Wallet } from "./wallet/Wallet";
 
 function App() {
   return (
     <>
-      <div className='flex flex-col h-screen'>
+      <div className='flex flex-col min-h-screen'>
+        <div className='flex-grow'>
         <Header />
-        <div className='flex-1'>
           <Router>
             <Routes>
               <Route path='/spot/:id' Component={SpotDetails}></Route>
               <Route path='/' Component={Home}></Route>
               <Route path='/create-listing' Component={CreateListing}></Route>
+              <Route path='/wallet' Component={Wallet}></Route>
             <Route path='/success' Component={SuccessMessage}></Route>
             </Routes>
           </Router>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       sans: ['"Raleway"', "sans-serif"],
