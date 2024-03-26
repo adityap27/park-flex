af747a929dae95382237b1127edee22d29c7485b
Ketul Patel <ketulpatel3012@gmail.com>
2024-03-26 10:24:26 -0300

    code cleanup

--- api/src/dao/connection.ts
+++ api/src/dao/connection.ts
@@ -1,20 +1,17 @@
 import mongoose from "mongoose";
-import { User, IUser } from "../models/User";
-import { Listing, IListing } from "../models/Listing";
+import { User } from "../models/User";
+import { Listing } from "../models/Listing";
 
-// export const getConnection = async () => {
-//   return await mongoose.connect(
-//     "mongodb+srv://group_user:Cy3H6QjOnrZ8XGYu@webgroupproject.dhqv6jf.mongodb.net/?retryWrites=true&w=majority&appName=WebGroupProject"
-//   );
-// };
-
-mongoose.connect("mongodb+srv://group_user:Cy3H6QjOnrZ8XGYu@webgroupproject.dhqv6jf.mongodb.net/?retryWrites=true&w=majority&appName=WebGroupProject")
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Connection error', error));
+mongoose
+  .connect(
+    "mongodb+srv://group_user:Cy3H6QjOnrZ8XGYu@webgroupproject.dhqv6jf.mongodb.net/?retryWrites=true&w=majority&appName=WebGroupProject"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Connection error", error));
 
 const dataBase = {
-  "listings": Listing,
-  "users": User 
+  listings: Listing,
+  users: User,
 };
 
-export {dataBase};
+export { dataBase };
