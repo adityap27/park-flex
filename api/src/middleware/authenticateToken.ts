018c2735f5b21f30bb4f68231227c1467da80990
jnrana <jy834177@dal.ca>
2024-03-28 01:20:23 -0300

    Middleware Update

--- api/src/middleware/authenticateToken.ts
+++ api/src/middleware/authenticateToken.ts
@@ -3,20 +3,20 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-// Extend the Express request object to include the user
+
 export interface AuthRequest extends Request {
-  user?: any; // Replace any with your user type, e.g., { _id: string; }
+  user?: any; 
 }
 
 export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+  const token = authHeader && authHeader.split(' ')[1]; 
 
   if (!token) return res.sendStatus(401);
 
   jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
     if (err) return res.sendStatus(403);
-    req.user = user; // Add the user payload to the request object
+    req.user = user; 
     next();
   });
 };
