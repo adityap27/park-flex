60a59ad7bf354880ab4ab8f5fe8bac4ee424aa1c
Ketul Patel <ketulpatel3012@gmail.com>
2024-03-29 10:55:44 -0300

    changed app icon

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,22 +1,36 @@
 import { Link } from "react-router-dom";
+import { FaSquareParking } from "react-icons/fa6";
 
 export const Footer = () => {
   return (
     <footer className='px-4 pb-2 pt-4 bg-footer'>
       <div className='flex flex-row'>
         <div className='flex flex-col flex-[2] md:flex-[3]'>
-          <p className='text-textSecondary'>ParkFlex</p>
+          <div className='flex flex-row items-center'>
+            <FaSquareParking fill='white' />
+            <p className='text-textSecondary'>arkFlex</p>
+          </div>
           <p className='text-textSecondary'>&copy;2024</p>
         </div>
         <div className='flex flex-col lg:flex-row justify-between flex-[2] xl:flex-[1.5] mx-6 my-2'>
-          <p className='text-textSecondary cursor-pointer'>Privacy & Terms</p>
-          <p className='text-textSecondary cursor-pointer'>Support</p>
+          <p className='text-textSecondary cursor-pointer hover:underline'>
+            Privacy & Terms
+          </p>
+          <p className='text-textSecondary cursor-pointer hover:underline'>
+            Support
+          </p>
           <Link to='/faq'>
-            <p className='text-textSecondary cursor-pointer'>FAQ</p>
+            <p className='text-textSecondary cursor-pointer hover:underline'>
+              FAQ
+            </p>
           </Link>
-          <p className='text-textSecondary cursor-pointer'>About</p>
+          <p className='text-textSecondary cursor-pointer hover:underline'>
+            About
+          </p>
           <Link to='/contact-us'>
-            <p className='text-textSecondary cursor-pointer'>Contact</p>
+            <p className='text-textSecondary cursor-pointer hover:underline'>
+              Contact
+            </p>
           </Link>
         </div>
       </div>
