f8cd1f725d7c1f69291eb9e38209a7836f9fe2f7
Shubham Vijaykumar Patel <sh659429@dal.ca>
2024-03-29 09:41:09 -0300

    added db credentials in env

--- api/src/dao/connection.ts
+++ api/src/dao/connection.ts
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 import { Users } from "../models/User";
 import { Listing } from "../models/Listing";
 import { Review } from "../models/Review";
+import dotenv from 'dotenv';
+dotenv.config();
 
 mongoose
   .connect(
-    "mongodb+srv://group_user:Cy3H6QjOnrZ8XGYu@webgroupproject.dhqv6jf.mongodb.net/?retryWrites=true&w=majority&appName=WebGroupProject"
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@webgroupproject.dhqv6jf.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority&appName=WebGroupProject`
   )
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Connection error", error));
