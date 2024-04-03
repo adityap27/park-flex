f8224068a6458c3744e4c0c31d329bbfbb245d3d
Ketul Patel <ketulpatel3012@gmail.com>
2024-04-03 10:14:26 -0300

    Minor changes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+/**
+Author : Ketul Patel
+**/
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>
 );
 
